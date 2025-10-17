657d8a67700ece15b982cc8fc7cecaae2e9be5c2
Maxcodingwork <104746670+Maxcodingwork@users.noreply.github.com>
2025-10-17 11:05:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// script.js
+// 資源位 SDK
+
+const sdk = new GmpResourceSDK({ 
+    host: 'https://ma.altatech.tw', 
+    appid: 8, // number
+    uuid: '578237563093909526', 
+    idType: 'baseid', // 根據實際情況設置
+    webId: '578237563093909526'
+});
+
+sdk.getResourceList(['7606814bd9d27b3fd5b191448fa61516']).then((resourceList) => {
+    // 根据返回的资源位列表进行自渲染工作
+    // 回调中的resourceList为ResourceData[]类型，详见资源位数据模型3.1.1
+});
+
+
+
 
 // 1. 取得需要操作的 DOM 元素
 const openModalBtn = document.getElementById('open-modal-btn');
